42dbbf705857c4f7a89a728c671a4073231f2a97
jocelflores <jocelynflores@utexas.edu>
2020-10-03 19:35:06 -0500

    put progress in folder, will select final and label as final

---
+++ EmployeeSQL/Query Progress/EmployeeSQL_Fourth Query.sql
@@ -0,0 +1,55 @@
+-- CREATE TABLE departments(
+-- 			dept_no VARCHAR PRIMARY KEY,
+-- 			dept_name VARCHAR)
+
+-- CREATE TABLE dept_emp(
+-- 			emp_no INTEGER REFERENCES employees(emp_no),
+-- 			dept_no VARCHAR REFERENCES departments(dept_no))
+
+-- CREATE TABLE dept_manager(
+-- 			dept_no VARCHAR REFERENCES departments(dept_no),
+-- 			emp_no INTEGER REFERENCES employees(emp_no))
+
+-- CREATE TABLE employees(
+-- 			emp_no INTEGER PRIMARY KEY,
+-- 			emp_title VARCHAR REFERENCES titles(title_id),
+-- 			birth_date VARCHAR,
+-- 			first_name VARCHAR,
+-- 			last_name VARCHAR,
+-- 			sex VARCHAR,
+-- 			hire_date VARCHAR)
+
+-- ALTER TABLE employees
+-- ADD PRIMARY KEY (emp_no)
+
+-- CREATE TABLE salaries(
+-- 			emp_no INTEGER REFERENCES employees(emp_no),
+-- 			salary INTEGER)
+
+-- CREATE TABLE titles(
+-- 			title_id VARCHAR PRIMARY KEY,
+-- 			title VARCHAR)
+
+-- SELECT * FROM employees
+-- SELECT * FROM titles
+-- SELECT * FROM dept_emp
+
+-- SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+-- FROM employees
+-- INNER JOIN salaries ON employees.emp_no=salaries.emp_no
+
+-- SELECT employees.last_name, employees.first_name, employees.hire_date
+-- FROM employees WHERE hire_date like '%1986%'
+
+-- SELECT dept_manager.emp_no, dept_manager.dept_no, employees.first_name, employees.last_name, departments.dept_name
+-- FROM employees
+-- JOIN dept_manager ON employees.emp_no=dept_manager.emp_no
+-- JOIN departments ON departments.dept_no=departments.dept_no
+
+-- SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name
+-- FROM employees
+-- JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+-- JOIN departments ON departments.dept_no=departments.dept_no
+
+SELECT * FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%'
